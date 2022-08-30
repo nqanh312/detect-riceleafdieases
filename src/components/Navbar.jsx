e517534d0ed8a0f72bcd6a30ea76f397e089cf07
nqanh312@gmail.com <nqanh312@gmail.com>
2022-08-30 22:40:15 +0700

    fix hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,10 +17,10 @@ const Navbar = () => {
                 <div className='flex items-center'>
                     <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>AcademicKnights</h1>
                     <ul className='hidden md:flex'>
-                        <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-                        <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-                        <li><Link to="allInOne" smooth={true} offset={-200} duration={500}>Detect</Link></li>
-                        <li><Link to="footer" smooth={true} offset={-100} duration={500}>Contact Us</Link></li>
+                        <li><Link className="cursor-pointer" to="home" smooth={true} duration={500}>Home</Link></li>
+                        <li><Link className="cursor-pointer" to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+                        <li><Link className="cursor-pointer" to="allInOne" smooth={true} offset={-200} duration={500}>Detect</Link></li>
+                        <li><Link className="cursor-pointer" to="footer" smooth={true} offset={-100} duration={500}>Contact Us</Link></li>
                     </ul>
                 </div>
                 <div className='hidden md:flex pr-4'>
@@ -28,16 +28,16 @@ const Navbar = () => {
                     <button className='px-8 py-3'>Sign up</button>
                 </div>
                 <div className='md:hidden mr-4' onClick={handleClick}>
-                    {!nav ? <FiMenu className='w-5' /> : <CgClose className='w-5' />}
+                    {!nav ? <FiMenu className='w-5 cursor-pointer' /> : <CgClose className='w-5 cursor-pointer' />}
 
                 </div>
             </div>
 
             <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="allInOne" smooth={true} offset={-200} duration={500}>Detect now</Link></li>
-                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="footer" smooth={true} offset={-100} duration={500}>Contact Us</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full cursor-pointer'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full cursor-pointer'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full cursor-pointer'><Link onClick={handleClose} to="allInOne" smooth={true} offset={-200} duration={500}>Detect now</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full cursor-pointer'><Link onClick={handleClose} to="footer" smooth={true} offset={-100} duration={500}>Contact Us</Link></li>
                 <div className='flex flex-col my-4'>
                     <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
                     <button className='px-8 py-3'>Sign Up</button>
