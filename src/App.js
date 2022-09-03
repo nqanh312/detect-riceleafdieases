ed8bc1526cee37ddf221eef2f74730eb65407e68
nqanh312@gmail.com <nqanh312@gmail.com>
2022-09-03 14:56:10 +0700

    run dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import NavbarWeb from './components/NavbarWeb'
 import NavbarSign from './components/NavbarSign'
 import HomePage from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+
+
+import Home from "./pages/adminpage/home/Home";
+// import Login from "./pages/login/Login";
+import List from "./pages/adminpage/list/List";
+import Single from "./pages/adminpage/single/Single";
+import New from "./pages/adminpage/new/New";
+import { productInputs, userInputs } from "./formSource";
+import { useContext } from "react";
+
 
 import React, { useState } from 'react'
 // import { Link, animateScroll as scroll } from 'react-scroll'
@@ -22,8 +31,9 @@ function App() {
 
   return (
     <>
+    <Home />
 
-      <div className='z-20 w-screen h-[80px] bg-white fixed'>
+      {/* <div className='z-20 w-screen h-[80px] bg-white fixed'>
         <div className='px-2 flex justify-between items-center w-full h-full'>
           <div className='flex items-center'>
             <h1 className='text-3xl font-bold mr-4 sm:text-4xl'><Link className="cursor-pointer" to="home" smooth={true} duration={500}>AcademicKnights</Link></h1>
@@ -32,7 +42,17 @@ function App() {
             <button className='border-none bg-transparent text-black mr-4'><Link className="cursor-pointer" to="login" smooth={true} duration={0}>Sign In</Link></button>
             <button className='px-8 py-3'><Link className="cursor-pointer" to="signup" smooth={true} duration={0}>Sign Up</Link></button>
           </div>
+          <div className='md:hidden mr-4' onClick={handleClick}>
+            {!nav ? <FiMenu className='w-5 cursor-pointer' /> : <CgClose className='w-5 cursor-pointer' />}
+          </div>
         </div>
+
+        <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+          <div className='flex flex-col my-4 md:hidden'>
+            <button className='px-8 py-3 mb-4 bg-transparent text-green-900 border-green-900 hover:border-green-600'><Link className="cursor-pointer" to="login" smooth={true} duration={0}>Sign In</Link></button>
+            <button className='px-8 py-3'><Link className="cursor-pointer" to="signup" smooth={true} duration={0}>Sign Up</Link></button>
+          </div>
+        </ul>
       </div>
 
 
@@ -40,18 +60,9 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-      </Routes>
+      </Routes> */}
 
-      {/* <Navbar/> */}
-      {/* <Login/> */}
-      {/* <NavbarSign/> */}
-      {/* <HomePage /> */}
 
-      {/* <Navbar/>
-      <Hero />
-      <About />
-      <Detect />
-      <Footer /> */}
     </>
   );
 }
---
+++ src/pages/adminpage/home/Home.jsx
@@ -0,0 +1,34 @@
+import Sidebar from "../../../components/admin/sidebar/Sidebar";
+import Navbar from "../../../components/admin/navbar/Navbar";
+import "./home.scss";
+import Widget from "../../../components/admin/widget/Widget";
+import Featured from "../../../components/admin/featured/Featured";
+import Chart from "../../../components/admin/chart/Chart";
+import Table from "../../../components/admin/table/Table";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
